cc86e49e25acf7e6fd8b85df4d26bd6b6accea42
PRASANTH S <119522531+PRASANTH2355@users.noreply.github.com>
2022-12-03 20:05:01 +0530

    Update rockPaperScissorPrakash.py

--- 3.12.2022/rockPaperScissor./rockPaperScissorPrakash.py
+++ 3.12.2022/rockPaperScissor./rockPaperScissorPrakash.py
@@ -1,22 +1,22 @@
 import random
 n=input("enter your choise: ")
-ist1 = ['rock', 'paper', 'sizor']
+ist1 = ['rock', 'paper', 'sissor']
 x=random.choice(ist1)
 print("computer's choise:",x)
 chos=x
 if n==chos and chos==n:
     print("draw")
-elif n=='rock' and  chos=='sizor':
+elif n=='rock' and  chos=='sissor':
     print("you win")
 elif n=='rock'and chos=='paper' :
     print("you lose")   
-elif n=='paper' and chos=='sizor':
+elif n=='paper' and chos=='sissor':
     print("you lose")
 elif n=='paper' and chos=='rock':
     print("you win")
-elif n=='sizor' and chos=='rock':
+elif n=='sissor' and chos=='rock':
     print("you fail")
-elif n=='sizor' and chos=='paper':
+elif n=='sissor' and chos=='paper':
     print("you win")
 else:
   print("enter valid input ")
