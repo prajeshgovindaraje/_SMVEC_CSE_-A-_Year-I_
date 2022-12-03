f397c224eb210e9896159511bf1b61f4b68b45b1
Santhosh D <santhoshdhananjeyan@gmail.com>
2022-12-03 20:05:58 +0530

    Update exno9(b).py

--- LabExercises/exno9(b).py
+++ LabExercises/exno9(b).py
@@ -1,5 +1,15 @@
-
-arr=['a','b','c','d',1,2,3,4]
+a=['12','123.32','adsd','234','edfre']
+print(a)
+b=input("Enter the type of value: ")
+if b=='str':
+    b=input("Enter the value: ")
+elif b=='int':
+    b=input()
+elif b=='float':
+    b=input()
+for i in a:
+    if i==b:
+        print("index",a.index(b))
 
          
            
